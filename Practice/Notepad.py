b4ece38042c8e0048ef91e4d75c7a4a4a429ad5f
i-meant-to-be <csm06063@naver.com>
2021-02-08 14:36:50 +0000

    Updated on 23:36, February 8th, 2021.

--- Practice/Notepad.py
+++ Practice/Notepad.py
@@ -2,18 +2,9 @@ from sys import stdin
 
 input = stdin.readline
 
-sentence = input().rstrip()
-alphabet = ["c=", "c-", "d-", "lj", "nj", "s=", "z="]
-count = 0
-pointer = 0
-
-while pointer != len(sentence):
-    if sentence[pointer:pointer + 3] == "dz=":
-        pointer = pointer + 3
-    elif alphabet.count(sentence[pointer:pointer + 2]):
-        pointer = pointer + 2
-    else:
-        pointer = pointer + 1
-    count += 1
-
-print(count)
+string = input().strip()
+if len(string) == 0:
+    count = 0
+else:
+    count = string.count(" ") + 1
+print(f"{count}")
