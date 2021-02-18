cdfbe06adcaf0a45c3e40a8230cfc11def694963
i-meant-to-be <csm06063@naver.com>
2021-02-18 14:40:49 +0000

    Updated at 23:40, February 18th, 2021.

--- Practice/Notepad.py
+++ Practice/Notepad.py
@@ -1,23 +1,7 @@
 from sys import stdin
+from itertools import combinations
 
 input = stdin.readline
-num = int(input())
-deque = list()
-for _ in range(num):
-    command = input().rstrip().split()
-    if command[0] == "size":
-        print(len(deque))
-    elif command[0] == "empty":
-        print(1 if not len(deque) else 0)
-    elif command[0] == "front":
-        print(deque[0] if len(deque) else -1)
-    elif command[0] == "back":
-        print(deque[-1] if len(deque) else -1)
-    elif command[0] == "pop_front":
-        print(deque.pop(0) if len(deque) else -1)
-    elif command[0] == "pop_back":
-        print(deque.pop() if len(deque) else -1)
-    elif command[0] == "push_front":
-        deque.insert(0, int(command[1]))
-    else:
-        deque.append(int(command[1]))
+num, target = [int(_) for _ in input().split()]
+cards = [int(_) for _ in input().split()]
+print(max(sum(case) for case in combinations(cards, 3) if sum(case) <= target))
